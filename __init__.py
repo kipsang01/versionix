9a60098eaf57132a86e527226973008fd94e25dd
kipsang <kipsang1998@gmail.com>
2024-12-13 22:37:00 +0300

    fix: fix the package to be pip installable

--- __init__.py
+++
@@ -1,4 +0,0 @@
-from versionix.vsx import Versionix
-from versionix.cli import main
-
-__all__ = ['Versionix', 'main']
--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import find_packages, setup
 
 setup(
     name='versionix',
@@ -8,9 +8,11 @@ setup(
             'vsx=versionix.cli:main',
         ],
     },
+    packages=find_packages(),
     author='Enock Kipsang',
     author_email='kipsang.dev@gmail.com',
     description='A lightweight distributed version control system',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
+    url="https://github.com/kipsang01/versionix",
 )

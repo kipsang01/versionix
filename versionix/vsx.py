278776d85a9307a76d1bd9f4fc05f7e779ef6803
kipsang <kipsang1998@gmail.com>
2024-12-13 21:55:00 +0300

    wip: add method to merge branches

--- versionix/vsx.py
+++ versionix/vsx.py
@@ -416,3 +416,193 @@ class Versionix:
 
         with open(branch_path, 'w') as f:
             json.dump(branch_metadata, f, indent=2)
+
+    def merge(self, source_branch: str, target_branch: str = None):
+        """
+        Merge source branch into target branch (current branch if not specified)
+
+        :param source_branch: Name of the branch to merge from
+        :param target_branch: Name of the branch to merge into (defaults to current branch)
+        """
+        branch_manager = BranchManager(self.repo_path)
+        if target_branch is None:
+            target_branch = branch_manager.get_current_branch()
+
+        source_branch_path = os.path.join(self.vsx_dir, 'branches', source_branch)
+        target_branch_path = os.path.join(self.vsx_dir, 'branches', target_branch)
+
+        if not os.path.exists(source_branch_path):
+            raise ValueError(f"Source branch {source_branch} does not exist")
+
+        if not os.path.exists(target_branch_path):
+            raise ValueError(f"Target branch {target_branch} does not exist")
+
+        with open(source_branch_path, 'r') as f:
+            source_branch_metadata = json.load(f)
+
+        with open(target_branch_path, 'r') as f:
+            target_branch_metadata = json.load(f)
+
+        with open(os.path.join(self.vsx_dir, 'commits'), 'r') as f:
+            all_commits = json.load(f)
+
+        def find_common_ancestor(source_commits, target_commits):
+            """
+            Find the common ancestor commit between two branches
+            """
+            source_commit_set = set(source_commits)
+            target_commit_set = set(target_commits)
+            common_commits = source_commit_set.intersection(target_commit_set)
+
+            if not common_commits:
+                return None
+
+            # Find the most recent common commit
+            for commit in reversed(target_commits):
+                if commit in common_commits:
+                    return commit
+
+            return None
+
+        common_ancestor = find_common_ancestor(
+            source_branch_metadata['commit_history'],
+            target_branch_metadata['commit_history']
+        )
+
+        def get_file_state_at_commit(commit_id):
+            """
+            Get file states for a specific commit
+            """
+            commit = next((c for c in all_commits if c['id'] == commit_id), None)
+            if not commit:
+                return {}
+
+            file_state = {}
+            for file_info in commit.get('files', []):
+                file_state[file_info['path']] = {
+                    'operation': file_info.get('operation', 'add'),
+                    'hash': file_info.get('hash')
+                }
+            return file_state
+
+        ancestor_state = get_file_state_at_commit(common_ancestor) if common_ancestor else {}
+        source_state = get_file_state_at_commit(source_branch_metadata['head'])
+        target_state = get_file_state_at_commit(target_branch_metadata['head'])
+
+        conflicts = {}
+        merged_files = {}
+
+        for filepath in set(list(ancestor_state.keys()) +
+                            list(source_state.keys()) +
+                            list(target_state.keys())):
+            ancestor_file = ancestor_state.get(filepath, {})
+            source_file = source_state.get(filepath, {})
+            target_file = target_state.get(filepath, {})
+
+            if (source_file.get('operation') == 'delete' and
+                    target_file.get('operation') != 'delete' and
+                    target_file.get('hash') != ancestor_file.get('hash')):
+                conflicts[filepath] = 'deleted_in_source_modified_in_target'
+
+            elif (target_file.get('operation') == 'delete' and
+                  source_file.get('operation') != 'delete' and
+                  source_file.get('hash') != ancestor_file.get('hash')):
+                conflicts[filepath] = 'deleted_in_target_modified_in_source'
+
+            elif (source_file.get('operation') != 'delete' and
+                  target_file.get('operation') != 'delete' and
+                  source_file.get('hash') != target_file.get('hash') and
+                  source_file.get('hash') != ancestor_file.get('hash') and
+                  target_file.get('hash') != ancestor_file.get('hash')):
+                conflicts[filepath] = 'modified_differently'
+
+            else:
+                if source_file.get('operation') == 'delete':
+                    merged_files[filepath] = {'operation': 'delete'}
+                elif target_file.get('operation') == 'delete':
+                    merged_files[filepath] = {'operation': 'delete'}
+                elif source_file.get('hash'):
+                    merged_files[filepath] = {
+                        'operation': 'modify',
+                        'hash': source_file['hash']
+                    }
+                elif target_file.get('hash'):
+                    merged_files[filepath] = {
+                        'operation': 'modify',
+                        'hash': target_file['hash']
+                    }
+
+        if conflicts:
+            print("Merge conflicts detected:")
+            for filepath, conflict_type in conflicts.items():
+                print(f"  {filepath}: {conflict_type}")
+
+            for filepath in conflicts:
+                self._create_conflict_markers(filepath,
+                                              source_state.get(filepath, {}),
+                                              target_state.get(filepath, {}))
+
+            return False
+
+        merge_commit = {
+            'id': hashlib.sha256(
+                f"Merge {source_branch} into {target_branch}".encode() +
+                str(time.time()).encode()
+            ).hexdigest(),
+            'message': f"Merge {source_branch} into {target_branch}",
+            'files': [
+                {**file_info, 'path': path}
+                for path, file_info in merged_files.items()
+            ],
+            'parent': [target_branch_metadata['head'], source_branch_metadata['head']]
+        }
+
+        with open(os.path.join(self.vsx_dir, 'commits'), 'r') as f:
+            commits = json.load(f)
+
+        commits.append(merge_commit)
+
+        with open(os.path.join(self.vsx_dir, 'commits'), 'w') as f:
+            json.dump(commits, f, indent=2)
+
+        target_branch_metadata['head'] = merge_commit['id']
+        target_branch_metadata['commit_history'].append(merge_commit['id'])
+
+        with open(target_branch_path, 'w') as f:
+            json.dump(target_branch_metadata, f, indent=2)
+
+        print(f"Successfully merged {source_branch} into {target_branch}")
+        return True
+
+    def _create_conflict_markers(self, filepath: str, source_file: dict, target_file: dict):
+        """
+        Create conflict markers in files with conflicting changes
+
+        :param filepath: Path to the file
+        :param source_file: File state from source branch
+        :param target_file: File state from target branch
+        """
+        full_path = os.path.join(self.repo_path, filepath)
+
+        def get_file_content(file_state):
+            if file_state.get('hash'):
+                object_path = os.path.join(self.vsx_dir, 'objects', file_state['hash'])
+                with open(object_path, 'r') as f:
+                    return f.read()
+            return ''
+
+        source_content = get_file_content(source_file)
+        target_content = get_file_content(target_file)
+
+        conflict_content = (
+            f"<<<<<<< HEAD (Current Branch)\n"
+            f"{target_content}"
+            f"=======\n"
+            f"{source_content}"
+            f">>>>>>> (Incoming Branch)\n"
+        )
+
+        with open(full_path, 'w') as f:
+            f.write(conflict_content)
+
+        print(f"Conflict markers added to {filepath}")
